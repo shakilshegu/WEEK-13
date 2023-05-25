9670dfcfe85afb2414796d64350d15b81e654e3c
shakilshagu <muhammadshakil1968@gmail.com>
2023-05-25 21:15:03 +0530

    new

--- sorting/insertion.js
+++ sorting/insertion.js
@@ -17,7 +17,7 @@
 let  m =[1,24,5,7,8,3,231,3,5]
 
 for(i=1;i<m.length;i++){
-    let temp =m[i]
+    let temp = m[i]
     let j = i-1
     while (j>=0 && m[j]>temp) {
         m[j+1] = m[j]
--- sorting/mersgesort.js
+++ sorting/mersgesort.js
@@ -6,13 +6,11 @@ function mergeSort(arr) {
   const mid = Math.floor(arr.length / 2);
   const leftArr = arr.slice(0, mid);
   const rightArr = arr.slice(mid);
-
   return merge(mergeSort(leftArr), mergeSort(rightArr));
 }
 
 function merge(leftArr, rightArr) {
   const sortedArr = [];
-
   while (leftArr.length && rightArr.length) {
     if (leftArr[0] <= rightArr[0]) {
       sortedArr.push(leftArr.shift());
